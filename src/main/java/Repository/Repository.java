98c70c05dcdd991c3e67a5d84102dc0a7b66ebe3
George Zacharias <zachariasg7@hotmail.gr>
2025-12-01 14:25:35 +0200

    Remove tracked binaries and respect .gitignore

--- src/main/java/Repository/Repository.java
+++ src/main/java/Repository/Repository.java
@@ -1,4 +1,5 @@
 package Repository;
+
 import java.util.List;
 
 /**
@@ -12,32 +13,34 @@ import java.util.List;
  * - Separates data access logic from business logic
  * - Hides implementation details (API, database, file, etc.)
  * - Makes code easier to test (can create mock repositories)
- * - Makes it easy to swap data sources (change from API to database without changing business logic)
+ * - Makes it easy to swap data sources (change from API to database without
+ * changing business logic)
  * 
  * GENERIC TYPE <T>:
  * T is a placeholder for any type (Cocktail, CocktailDetails, etc.)
  * This makes the interface reusable for different data types
- * 
-
+ *
+ */
 public interface Repository<T> {
-    
     /**
      * Retrieves an object by its index or ID
      * 
      * @param index Position or unique identifier of the object
      * @return Object of type T, or null if not found
      * 
-     * Example: repository.getObject(5) might return the 5th cocktail or cocktail with ID 5
+     *         Example: repository.getObject(5) might return the 5th cocktail or
+     *         cocktail with ID 5
      */
     T getObject(int index);
-    
+
     /**
      * Removes an object from the repository
      * 
      * @param t The object to remove
      * 
-     * Note: Currently just sets the object to null in implementations
-     * Could be expanded to actually delete from a database or API
+     *          Note: Currently just sets the object to null in implementations
+     *          Could be expanded to actually delete from a database or API
      */
     void remove(T t);
+
 }
