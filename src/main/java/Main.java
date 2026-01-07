0bfc2af632aaae3d8590f9f84d869a574703d5ca
George Zacharias <zachariasg7@hotmail.gr>
2026-01-07 14:19:08 +0200

    fixed maven compile error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,42 @@
 import OS.LaunchPage;
 import OS.OSFactory;
 import ObserverPattern.*;
-
 import java.util.List;
 
+// how to run project
+// mvn compile
+// mvn exec:java
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Create the Subject object
         // It will handle updating all observers
         // as well as deleting and adding the
         // Create an Observer that will be sent updates from Subject
 
-
-
         OSFactory OSFactory = new OSFactory();
         LaunchPage launchPage = new LaunchPage();
 
-
-        /*for(Favorite favorite : favoriteDao.getAllFavorites()){
-            System.out.println("Favorites: [ID: " + favorite.getID() + ", Name: " + favorite.getName() + " Image: " + favorite.getImage() + " ]");
-        }*/
-/*        FavoriteDaoImplementation daoImplementation = new FavoriteDaoImplementation();
-        *//*Favorite favorite = Favorite.Relax;
-        daoImplementation.createFavorite("155 Belmont", favorite);
-        daoImplementation.createFavorite("Mojito", Favorite.Energizing);*//*
-        daoImplementation.createFavorite("hello",Favorite.Energizing);*/
-        /*FavoriteView favoriteView= new FavoriteView();
-        FavoriteController favoriteController= new FavoriteController(favoriteView);
-        favoriteController.updateView();*/
+        /*
+         * for(Favorite favorite : favoriteDao.getAllFavorites()){
+         * System.out.println("Favorites: [ID: " + favorite.getID() + ", Name: " +
+         * favorite.getName() + " Image: " + favorite.getImage() + " ]");
+         * }
+         */
+        /*
+         * FavoriteDaoImplementation daoImplementation = new
+         * FavoriteDaoImplementation();
+         *//*
+            * Favorite favorite = Favorite.Relax;
+            * daoImplementation.createFavorite("155 Belmont", favorite);
+            * daoImplementation.createFavorite("Mojito", Favorite.Energizing);
+            *//*
+               * daoImplementation.createFavorite("hello",Favorite.Energizing);
+               */
+        /*
+         * FavoriteView favoriteView= new FavoriteView();
+         * FavoriteController favoriteController= new FavoriteController(favoriteView);
+         * favoriteController.updateView();
+         */
     }
 }
